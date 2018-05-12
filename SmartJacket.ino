fdb9fb24a61f0d1c92b1227d074ffdd56eba7a98
artvorob <37974395+artiomvor06@users.noreply.github.com>
2018-05-12 17:32:41 +0400

    +accel

--- SmartJacket.ino
+++ SmartJacket.ino
@@ -189,6 +189,34 @@ int TempOut() {
   int tempOut1 = round(float(sensorsTempOutPin.getTempCByIndex(0)));
   digitalWrite(powerTempOutPin, LOW);
   return tempOut1;
+  
+}
+
+//акселерометр
+void acsel() {
+long int t = millis();
+  if ( t_next < t ) {
+    int16_t ax_raw, ay_raw, az_raw, gx_raw, gy_raw, gz_raw;
+    float ay, gx
+
+    t_next = t + T_OUT;
+    accel.getMotion6(&ax_raw, &ay_raw, &az_raw, &gx_raw, &gy_raw, &gz_raw);
+
+    // сырые данные акселерометра нужно преобразовать в единицы гравитации
+    // при базовых настройках 1G = 4096
+    ay = ay_raw / 4096.0;
+    // на случай, если на акселерометр действуют посторонние силы, которые могут
+    // увеличить ускорение датчика свыше 1G, установим границы от -1G до +1G
+    ay = clamp(ay, -1.0, 1.0);
+
+    // функция acos возвращает угол в радианах, так что преобразуем
+    // его в градусы при помощи коэффициента TO_DEG
+    if ( ay >= 0) {
+      angle_ax = 90 - TO_DEG * acos(ay);
+    } else {
+      angle_ax = TO_DEG * acos(-ay) - 90;
+    }
+  }
 }
 
 // Получение координат
